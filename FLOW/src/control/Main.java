58c85278186ef7f23b53dbe7a0f2f7426875c371
jwall <jwall@DESKTOP-LJ079A6.fritz.box>
2017-04-20 17:02:13 +0200

    new Stuff

--- FLOW/src/control/Main.java
+++ FLOW/src/control/Main.java
@@ -1,21 +1,54 @@
 package control;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("ComControl");
+
+		initRootLayout();
+
+		showFlowScene();
+	}
+	
+	public void initRootLayout() {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getClassLoader().getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void showFlowScene() {
+		try {
+			FXMLLoader flowSceneLoader = new FXMLLoader();
+			flowSceneLoader
+					.setLocation(Main.class.getClassLoader().getResource("view/FlowScene.fxml"));
+			AnchorPane flowScene = (AnchorPane) flowSceneLoader.load();
+
+			rootLayout.setCenter(flowScene);
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
---
+++ FLOW/src/view/FlowSceneController.java
@@ -0,0 +1,46 @@
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class FlowSceneController implements Initializable {
+
+	@FXML
+	private AnchorPane anchor;
+	
+	@FXML
+	private Canvas canvas;
+	
+	@FXML
+	private GraphicsContext gc;
+	
+	@FXML
+	private ComboBox<String> computationTypeChooser;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		gc = canvas.getGraphicsContext2D();
+		
+		//init ComboBox
+		computationTypeChooser.getItems().addAll("Kanalsystem", "Social Network Analysis");
+		computationTypeChooser.setOnMousePressed(new EventHandler<MouseEvent>(){
+		    @Override
+		    public void handle(MouseEvent event) {
+		    	computationTypeChooser.requestFocus();
+		    }
+		});
+		computationTypeChooser.setValue("Kanalsystem");
+	}
+
+	
+}
